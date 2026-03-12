e1ed6df20c1d637f132fe588125052b78d9dfe1c
fathannnur <carelius02@gmail.com>
2026-03-12 10:40:12 +0700

    Menambahkan insertsort

--- file1.cpp
+++ file1.cpp
@@ -8,3 +8,49 @@ void input()
 {
     while (true)
     {
+            cout << "Masukan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray Tidak Boleh Lebih Dari 20\n";
+        }
+    }
+    cout << endl;
+    cout << "=============================================";
+    cout << "==========Masukan Elemen Array==============";
+    cout << "=============================================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
+
+void insertionsort()
+{
+    int temp;
+    int j;
+
+    for (int i = 0; i < n; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+        cout << "\nstep" << i << ": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+    }
+}
